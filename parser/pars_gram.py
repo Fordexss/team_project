99f43fae1fdbdb60314b364f78ef691aff66c988
unknown <gavrisnazar981@gmail.com>
2023-10-11 22:46:22 +0300

    pars lex and gram

--- parser/pars_gram.py
+++ parser/pars_gram.py
@@ -1,75 +1,79 @@
 import requests
 from bs4 import BeautifulSoup
-from aiogram import Dispatcher
+import sqlite3
 
-additionally = {}
+conn = sqlite3.connect('data.db')
 
-async def pars_gram(dp: Dispatcher):
-    forA1 = ['Простий майбутній час', 'Простий минулий час', 'Простий теперішній час', 'Негативні речення', 'Види запитань', 'Певний артикль',
+cr = conn.cursor()
+cr.execute("""CREATE TABLE IF NOT EXISTS grammar(
+        theme TEXT,
+        text TEXT,
+        video TEXT,
+        level VARCHAR(12)
+    )""")
+
+forA1 = ['Простий майбутній час', 'Простий минулий час', 'Простий теперішній час', 'Негативні речення', 'Види запитань', 'Певний артикль',
          'Невизначений артикль',  'Присвійні займенники', ]
-    forA2 = ['Особисті займенники', 'Прикметник (Adjective)', 'Прислівник (Adverb)', 'Питальні займенники', 'Структура речення (Sentence Structure)',
+forA2 = ['Особисті займенники', 'Прикметник (Adjective)', 'Прислівник (Adverb)', 'Питальні займенники', 'Структура речення (Sentence Structure)',
             'Множина іменників', 'Умовні речення (Conditionals)', 'Умовні речення нульового типу', 'Присвійні займенники']
-    forB1 = ['Теперішнй тривалий час', 'Теперішнй доконаний час', 'Минулий тривалий час', 'Нульовий артикль', 'Прийменники з іншими значеннями',
+forB1 = ['Теперішнй тривалий час', 'Теперішнй доконаний час', 'Минулий тривалий час', 'Нульовий артикль', 'Прийменники з іншими значеннями',
             'Прийменники після дієслів і прикметників (фіксовані прийменники)', 'Умовні речення другого типу', 'Умовні речення першого типу',
             'Дієслово (Verb)', 'Типи дієслів', 'Сполучники (Conjunctions)', 'Присвійний відмінок іменників']
-    forB2 = ['Майбутній тривалий час', 'Майбутній доконаний час', 'Взаємні займенники', 'Невизначені займенники', 'Відносні займенники',
+forB2 = ['Майбутній тривалий час', 'Майбутній доконаний час', 'Взаємні займенники', 'Невизначені займенники', 'Відносні займенники',
             'Вказівні займенники', 'Зворотні займенники', 'Категорія роду іменників', 'Місце прислівника в реченні', 'Ступені порівняння прислівників', 
             'Ступені порівняння прикметників', 'Порядок прикметників перед іменниками', 'Майбутній тривалий час', 'Майбутній доконаний час',
             'Минулий доконаний час', 'Модальні дієслова (Modal Verbs)']
 
-    a1 = {}
-    a2 = {}
-    b1 = {}
-    b2 = {}
-    
-    res = requests.get('https://teacheng.info/theory/grammar')
-    soup =  BeautifulSoup(res.content, "html.parser")
-    items = soup.find('div', class_ = 'post-content')
-    for a in items.find_all('a'):
-        if a.text in forA1:
-            href = a.get("href")
-            link = 'https://teacheng.info/' + href
-            req = requests.get(link)
-            new_soup = BeautifulSoup(req.content, "html.parser")
-            theme = new_soup.find('h1', class_ = 'post-title')
-            text = new_soup.find('div', class_ = 'post-content')
-            a1[a.text] = {'text': text.text, 'theme': theme.text}
-        
-        elif a.text in forA2:
-            href = a.get("href")
-            link = 'https://teacheng.info/' + href
-            req = requests.get(link)
-            new_soup = BeautifulSoup(req.content, "html.parser")
-            theme = new_soup.find('h1', class_ = 'post-title')
-            text = new_soup.find('div', class_ = 'post-content')
-            a2[a.text] = {'text': text.text, 'theme': theme.text}
-        
-        elif a.text in forB1:
-            href = a.get("href")
-            link = 'https://teacheng.info/' + href
-            req = requests.get(link)
-            new_soup = BeautifulSoup(req.content, "html.parser")
-            theme = new_soup.find('h1', class_ = 'post-title')
-            text = new_soup.find('div', class_ = 'post-content')
-            b1[a.text] = {'text': text.text, 'theme': theme.text}
+res = requests.get('https://teacheng.info/theory/grammar')
+soup =  BeautifulSoup(res.content, "html.parser")
+items = soup.find('div', class_ = 'post-content')
+for a in items.find_all('a'):
+    if a.text in forA1:
+        href = a.get("href")
+        link = 'https://teacheng.info/' + href
+        req = requests.get(link)
+        new_soup = BeautifulSoup(req.content, "html.parser")
+        theme = new_soup.find('h1', class_ = 'post-title')
+        text = new_soup.find('div', class_ = 'post-content')
+        cr.execute("""INSERT INTO grammar(level, theme, text) VALUES('A1', ?, ? )""", (theme.text, text.text))   
+                
             
-        elif a.text in forB2:
-            href = a.get("href")
-            link = 'https://teacheng.info/' + href
-            req = requests.get(link)
-            new_soup = BeautifulSoup(req.content, "html.parser")
-            theme = new_soup.find('h1', class_ = 'post-title')
-            text = new_soup.find('div', class_ = 'post-content')
-            b2[a.text] = {'text': text.text, 'theme': theme.text}
+    elif a.text in forA2:
+        href = a.get("href")
+        link = 'https://teacheng.info/' + href
+        req = requests.get(link)
+        new_soup = BeautifulSoup(req.content, "html.parser")
+        theme = new_soup.find('h1', class_ = 'post-title')
+        text = new_soup.find('div', class_ = 'post-content')
+        cr.execute("""INSERT INTO grammar(level, theme, text) VALUES('A2', ?, ? )""", (theme.text, text.text))   
         
-        else:
-            href = a.get("href")
-            link = 'https://teacheng.info/' + href
-            req = requests.get(link)
-            new_soup = BeautifulSoup(req.content, "html.parser")
-            theme = new_soup.find('h1', class_ = 'post-title')
-            text = new_soup.find('div', class_ = 'post-content')
-            additionally[a.text] = {'text': text.text, 'theme': theme.text}
+    elif a.text in forB1:
+        href = a.get("href")
+        link = 'https://teacheng.info/' + href
+        req = requests.get(link)
+        new_soup = BeautifulSoup(req.content, "html.parser")
+        theme = new_soup.find('h1', class_ = 'post-title')
+        text = new_soup.find('div', class_ = 'post-content')
+        cr.execute("""INSERT INTO grammar(level, theme, text) VALUES('B1', ?, ? )""", (theme.text, text.text))   
+            
+    elif a.text in forB2:
+        href = a.get("href")
+        link = 'https://teacheng.info/' + href
+        req = requests.get(link)
+        new_soup = BeautifulSoup(req.content, "html.parser")
+        theme = new_soup.find('h1', class_ = 'post-title')
+        text = new_soup.find('div', class_ = 'post-content')
+        cr.execute("""INSERT INTO grammar(level, theme, text) VALUES('B2', ?, ? )""", (theme.text, text.text))   
         
-    await dp.bot.send_message(text=a1["Присвійні займенники"]['text'], chat_id=1011382984, parse_mode="html")
+    else:
+        href = a.get("href")
+        link = 'https://teacheng.info/' + href
+        req = requests.get(link)
+        new_soup = BeautifulSoup(req.content, "html.parser")
+        theme = new_soup.find('h1', class_ = 'post-title')
+        text = new_soup.find('div', class_ = 'post-content')
+        cr.execute("""INSERT INTO grammar(level, theme, text) VALUES('additionally', ?, ? )""", (theme.text, text.text))   
         
+cr.execute("""SELECT * FROM grammar""")
+res = cr.fetchall()
+print(res)
--- parser/pars_lex.py
+++ parser/pars_lex.py
@@ -1,73 +1,77 @@
 import requests
 from bs4 import BeautifulSoup
-from aiogram import Dispatcher
+import sqlite3
 
+conn = sqlite3.connect('data.db')
 
+cr = conn.cursor()
+cr.execute("""CREATE TABLE IF NOT EXISTS lex(
+        word TEXT,
+        translate TEXT,
+        level VARCHAR(12)
+    )""")
 
-additionally_lex = {}
-async def pars_lex(dp: Dispatcher):
-    link='https://uk.speaklanguages.com/%D0%B0%D0%BD%D0%B3%D0%BB%D1%96%D0%B9%D1%81%D1%8C%D0%BA%D0%B0/%D0%BB%D0%B5%D0%BA%D1%81%D0%B8%D0%BA%D0%B0/#:~:text=%D0%90%D0%BD%D0%B3%D0%BB%D1%96%D0%B9%D1%81%D1%8C%D0%BA%D0%B0%20%D0%BB%D0%B5%D0%BA%D1%81%D0%B8%D0%BA%D0%B0%2C%20%D1%80%D0%BE%D0%B7%D0%B4%D1%96%D0%BB%D0%B5%D0%BD%D0%B0%20%D0%BD%D0%B0%2065%20%D1%89%D0%BE%D0%B4%D0%B5%D0%BD%D0%BD%D0%B8%D1%85%20%D1%82%D0%B5%D0%BC%2C%20%D1%96%D0%B7,%D0%B7%D0%B0%D0%BF%D0%B8%D1%81%D0%B0%D0%BD%D0%B8%D0%BC%20%D0%BD%D0%BE%D1%81%D1%96%D1%8F%D0%BC%D0%B8%20%D0%BC%D0%BE%D0%B2%D0%B8.%20%D0%9F%D0%BE%D0%B2%D0%BD%D1%96%D1%81%D1%82%D1%8E%20%D0%BF%D0%B5%D1%80%D0%B5%D0%BA%D0%BB%D0%B0%D0%B4%D0%B5%D0%BD%D0%BE%20%D0%BD%D0%B0%2035%20%D0%BC%D0%BE%D0%B2.'
-    req = requests.get(link)
+link='https://uk.speaklanguages.com/%D0%B0%D0%BD%D0%B3%D0%BB%D1%96%D0%B9%D1%81%D1%8C%D0%BA%D0%B0/%D0%BB%D0%B5%D0%BA%D1%81%D0%B8%D0%BA%D0%B0/#:~:text=%D0%90%D0%BD%D0%B3%D0%BB%D1%96%D0%B9%D1%81%D1%8C%D0%BA%D0%B0%20%D0%BB%D0%B5%D0%BA%D1%81%D0%B8%D0%BA%D0%B0%2C%20%D1%80%D0%BE%D0%B7%D0%B4%D1%96%D0%BB%D0%B5%D0%BD%D0%B0%20%D0%BD%D0%B0%2065%20%D1%89%D0%BE%D0%B4%D0%B5%D0%BD%D0%BD%D0%B8%D1%85%20%D1%82%D0%B5%D0%BC%2C%20%D1%96%D0%B7,%D0%B7%D0%B0%D0%BF%D0%B8%D1%81%D0%B0%D0%BD%D0%B8%D0%BC%20%D0%BD%D0%BE%D1%81%D1%96%D1%8F%D0%BC%D0%B8%20%D0%BC%D0%BE%D0%B2%D0%B8.%20%D0%9F%D0%BE%D0%B2%D0%BD%D1%96%D1%81%D1%82%D1%8E%20%D0%BF%D0%B5%D1%80%D0%B5%D0%BA%D0%BB%D0%B0%D0%B4%D0%B5%D0%BD%D0%BE%20%D0%BD%D0%B0%2035%20%D0%BC%D0%BE%D0%B2.'
+req = requests.get(link)
 
-    soup = BeautifulSoup(req.content, "html.parser")
+soup = BeautifulSoup(req.content, "html.parser")
     
-    forA1 = ['Меблі та господарські приладдя', 'Одяг та особисті речі', 'Кольори', 'Числа']
-    forA2 = ['Телефон', 'Як описати людину', 'Подорожування', 'Їжа', 'В ресторані', 'Гід по меню', 'Їжа та напої']
-    forB1 = ['Дні тижня', 'Місяці й пори року', 'Свята та фестивалі', 'Комп\'ютери та Інтернет', 'Академічні предмети', 'Освіта',
+forA1 = ['Меблі та господарські приладдя', 'Одяг та особисті речі', 'Кольори', 'Числа']
+forA2 = ['Телефон', 'Як описати людину', 'Подорожування', 'Їжа', 'В ресторані', 'Гід по меню', 'Їжа та напої']
+forB1 = ['Дні тижня', 'Місяці й пори року', 'Свята та фестивалі', 'Комп\'ютери та Інтернет', 'Академічні предмети', 'Освіта',
             'Сфери діяльності', 'Професії', 'Зайнятість', 'Спорт та ігри', 'Подорожування водним транспортом', 'Подорожування літаком',
             'Подорожування автобусом та поїздом', 'Автомобілі', 'Погода', 'Тіло людини']
-    forB2 = ['Телефон', 'Музичні інструменти', 'Покупки', 'У місті', 'Прохолоджувальні напої', 'Алкоголь', 'Частини машини', 'Погода',
+forB2 = ['Телефон', 'Музичні інструменти', 'Покупки', 'У місті', 'Прохолоджувальні напої', 'Алкоголь', 'Частини машини', 'Погода',
             'Родина', 'Континенти та регіони світу', 'Грошові одиниці', 'Мови', 'Господарські товари', 'Меблі та господарські приладдя', 'Тварини']
 
-    
-    a1_lex = {}
-    a2_lex = {}
-    b1_lex = {}
-    b2_lex = {}
-    for a in soup.find_all('a'):
-        if a.text in forA1:
-            link = a.get('href')
-            req = requests.get(link)
-            new_soup = BeautifulSoup(req.content, "html.parser")
-            for table in new_soup.find_all('table', class_='bilingual'):
-                for tr in table.find_all('tr'):
-                    if word_pairs:= tr.find_all('td'):
-                        a1_lex[word_pairs[0].text] = word_pairs[1].text
+for a in soup.find_all('a'):
+    if a.text in forA1:
+        link = a.get('href')
+        req = requests.get(link)
+        new_soup = BeautifulSoup(req.content, "html.parser")
+        for table in new_soup.find_all('table', class_='bilingual'):
+            for tr in table.find_all('tr'):
+                if word_pairs:= tr.find_all('td'):
+                    cr.execute("""INSERT INTO lex(level, word, translate) VALUES('A1', ?, ?)""", (word_pairs[0].text, word_pairs[1].text))
                         
-        elif a.text in forA2:
-            link = a.get('href')
-            req = requests.get(link)
-            new_soup = BeautifulSoup(req.content, "html.parser")
-            for table in new_soup.find_all('table', class_='bilingual'):
-                for tr in table.find_all('tr'):
-                    if word_pairs:= tr.find_all('td'):
-                        a2_lex[word_pairs[0].text] = word_pairs[1].text
+    elif a.text in forA2:
+        link = a.get('href')
+        req = requests.get(link)
+        new_soup = BeautifulSoup(req.content, "html.parser")
+        for table in new_soup.find_all('table', class_='bilingual'):
+            for tr in table.find_all('tr'):
+                if word_pairs:= tr.find_all('td'):
+                    cr.execute("""INSERT INTO lex(level, word, translate) VALUES('A2', ?, ?)""", (word_pairs[0].text, word_pairs[1].text))
+
                 
-        elif a.text in forB1:
-            link = a.get('href')
-            req = requests.get(link)
-            new_soup = BeautifulSoup(req.content, "html.parser")
-            for table in new_soup.find_all('table', class_='bilingual'):
-                for tr in table.find_all('tr'):
-                    if word_pairs:= tr.find_all('td'):
-                        b1_lex[word_pairs[0].text] = word_pairs[1].text
+    elif a.text in forB1:
+        link = a.get('href')
+        req = requests.get(link)
+        new_soup = BeautifulSoup(req.content, "html.parser")
+        for table in new_soup.find_all('table', class_='bilingual'):
+            for tr in table.find_all('tr'):
+                if word_pairs:= tr.find_all('td'):
+                    cr.execute("""INSERT INTO lex(level, word, translate) VALUES('B1', ?, ?)""", (word_pairs[0].text, word_pairs[1].text))
         
-        elif a.text in forB2:
-            link = a.get('href')
-            req = requests.get(link)
-            new_soup = BeautifulSoup(req.content, "html.parser")
-            for table in new_soup.find_all('table', class_='bilingual'):
-                for tr in table.find_all('tr'):
-                    if word_pairs:= tr.find_all('td'):
-                        b2_lex[word_pairs[0].text] = word_pairs[1].text
+    elif a.text in forB2:
+        link = a.get('href')
+        req = requests.get(link)
+        new_soup = BeautifulSoup(req.content, "html.parser")
+        for table in new_soup.find_all('table', class_='bilingual'):
+            for tr in table.find_all('tr'):
+                if word_pairs:= tr.find_all('td'):
+                    cr.execute("""INSERT INTO lex(level, word, translate) VALUES('B2', ?, ?)""", (word_pairs[0].text, word_pairs[1].text))
                         
-        else:
-            link = a.get('href')
-            req = requests.get(link)
-            new_soup = BeautifulSoup(req.content, "html.parser")
-            for table in new_soup.find_all('table', class_='bilingual'):
-                for tr in table.find_all('tr'):
-                    if word_pairs:= tr.find_all('td'):
-                        additionally_lex[word_pairs[0].text] = word_pairs[1].text
-    
-    await dp.bot.send_message(text=a1_lex['red'], chat_id=1011382984)        
+    else:
+        link = a.get('href')
+        req = requests.get(link)
+        new_soup = BeautifulSoup(req.content, "html.parser")
+        for table in new_soup.find_all('table', class_='bilingual'):
+            for tr in table.find_all('tr'):
+                if word_pairs:= tr.find_all('td'):
+                    cr.execute("""INSERT INTO lex(level, word, translate) VALUES('additionally', ?, ?)""", (word_pairs[0].text, word_pairs[1].text))
+                    
+cr.execute("""SELECT * FROM lex""")
+res = cr.fetchall()
+print(res)
+            
